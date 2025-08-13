db0d457ff53f38e4cc99e274fb83e072609d753a
Tolulope Osinubi <tolulope.osinubi@circleci.com>
2025-08-13 14:42:12 -0700

    Initial commit

---
+++ BartSchedule.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+
+	// Temporary usage to prevent import removal
+	_ = fmt.Sprintf
+	_ = http.StatusOK
+	_ = os.Getenv
+	_ = time.Now
+	_ = tea.NewProgram
+	// -----------
+
+	//
+
+}
